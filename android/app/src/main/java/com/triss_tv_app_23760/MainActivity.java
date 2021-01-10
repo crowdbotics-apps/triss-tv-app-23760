ee2924c26172d0e90e69e1b91978777f6591b53f
Crowdbotics <team@crowdbotics.com>
2021-01-10 23:15:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/triss_tv_app_23760/MainActivity.java
@@ -0,0 +1,15 @@
+package com.triss_tv_app_23760;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "triss_tv_app_23760";
+    }
+}
